7108c1f2970293d18c1270500836e3c74fb830fb
jhaharsh <82922456+jhaharsh03@users.noreply.github.com>
2022-12-24 08:32:45 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,18 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBbXVieLZ-AQAKW0YmVHuD9riTvEAGc7cM",
+  authDomain: "todo-4c6de.firebaseapp.com",
+  // databaseURL: "https://todotest-ea3ea-default-rtdb.firebaseio.com",
+  projectId: "todo-4c6de",
+  storageBucket: "todo-4c6de.appspot.com",
+  messagingSenderId: "683386871661",
+  appId: "1:683386871661:web:437c3465b4a75b062ac464",
+  measurementId: "G-NE1Y2TJX2F"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  var db = firebase.firestore();
+
+
+
+
---
+++ plan.md
@@ -0,0 +1,10 @@
+1. Create the boxes we need to create
+2. Cerate the banner Background image
+3. Create the header
+4. Create the input
+5. Create the to do item section 
+6. Set up out database 
+7. Create the ability to save data in a firebase database
+8. Create the functionalties to grab the data from the database
+9. Creat the functionalities to updata the data with the new status of completed or active 
+
